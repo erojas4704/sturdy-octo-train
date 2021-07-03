77dd0090374ba0b962a1fc6e186fb732c0c835c9
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-07-03 14:02:14 -0400

    More testing

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 const { calculateRangeOfAProjectile } = require("./app");
 
 
-describe("Should calculate the range of a projectile", () => {
+describe("Should calculate the range of a projectile on even ground where y is 0", () => {
     test("Test on even ground", () => {
         let result = calculateRangeOfAProjectile(10, 45, 0);
         expect(result).toEqual(10.2);
@@ -12,6 +12,15 @@ describe("Should calculate the range of a projectile", () => {
         expect(result).toEqual(8.1);
     });
 
+    test("Test on even ground 3", () => {
+        let result = calculateRangeOfAProjectile(44, 77, 0);
+        expect(result).toEqual(86.6);
+    });
+
+
+});
+
+describe("Should calculate the range of a projectile on uneven ground where y is not 0", () => {
     test("Test on uneven ground ", () => {
         let result = calculateRangeOfAProjectile(10, 45, 14);
         expect(result).toEqual(18.4);
---
+++ readme.md
@@ -0,0 +1,9 @@
+##Usage
+
+
+Command line:
+```
+node app.js -v velocity -a angle -h height
+```
+
+Where v is velocity, a is equal to angle, in degrees, and h is equal to height.
